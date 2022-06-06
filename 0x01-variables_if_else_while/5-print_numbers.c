60923b4d3ccaed5c00afccf7f08f91a2068bf2a3
BOUBEY <hamma.boubey@gmail.com>
2022-06-05 19:32:03 -0700

    task 5

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <unistd.h>
+
+/**
+ * main - printing numbers 
+ *
+ * Description: all single digit numbers of base 10 from 0
+ *
+ * Return: The value of the main function return is 0
+ */
+
+int main(void)
+{
+	int digit = 0;
+
+	while (digit < 10)
+	{
+		printf("%d", digit);
+	digit++;
+	}
+	if (digit == 9)
+	{
+		putchar('\n');
+	}
+	return (0);
+}
