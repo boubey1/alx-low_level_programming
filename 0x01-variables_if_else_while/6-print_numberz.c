b9d87e18abf1be3bad083a80f841d5763d616c94
BOUBEY <hamma.boubey@gmail.com>
2022-06-05 20:11:26 -0700

    task 6

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <unistd.h>
+
+/**
+ * main - printing single digit from 0 to 9
+ *
+ * Description: only using function puts
+ *
+ * Return: The value of the main function return is 0
+ */
+
+int main(void)
+{
+	int digit = 0;
+
+	while (digit < 10)
+	{
+		putchar(digit);
+		digit++;
+		if (digit == 10)
+		{
+			putchar('\n');
+		}
+	}
+	return (0);
+}
