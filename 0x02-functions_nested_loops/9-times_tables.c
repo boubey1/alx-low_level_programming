46487c2b2ad5278821354cabee975939522a682e
BOUBEY <hamma.boubey@gmail.com>
2022-06-07 23:19:48 -0700

    0x02 task 9

---
+++ 0x02-functions_nested_loops/9-times_tables.c
@@ -0,0 +1,37 @@
+#include "main.h"
+
+/**
+ * times_table - Print the times tables 0 to 9
+ *
+ */
+void jack_bauer(void)
+{
+	int number;
+	int mult;
+	int prod;
+
+	for (number = 0; number <= 9; number++)
+	{
+		_putchar('0');
+		
+		for (mult = 1; mult <= 9; mult++)
+		{
+			_putchar(',');
+			_putchar(' ');
+
+			prod = number * mult;
+
+			if (prod <= 9)
+			{
+				_putchar(' ');
+			}
+			else
+			{
+				_putchar((prod / 10) + '0');
+			}
+
+			_putchar((minutes % 10) + '0');
+		}
+		_putchar('\n');
+	}
+}
