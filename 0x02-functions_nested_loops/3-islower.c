017eeb2d011a4b30e42311835096fb0d3565e317
BOUBEY <hamma.boubey@gmail.com>
2022-06-07 21:06:05 -0700

    0x02 task 3

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * _islower
+ *
+ * Return: if a lowercase 1 ifnot 0
+ */
+int _islower(int c)
+{
+	if (c >= 'a' && c <= 'z')
+	{
+		return (0);
+	}
+	else
+	{
+		return (0);
+	}
+}
