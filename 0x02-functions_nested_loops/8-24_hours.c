5488e740e586ae37005ce72777ff6bd27885324f
BOUBEY <hamma.boubey@gmail.com>
2022-06-07 22:48:25 -0700

    0x02 task 8

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * jack_bauer - Print the minuts
+ *
+ */
+void jack_bauer(void)
+{
+	int time;
+	int minutes;
+	
+	for (time = 0; time <= 23; time++)
+	{
+		for (minutes = 0; minutes <= 59; minutes++)
+		{
+			_putchar((time / 10) + '0');
+			_putchar((time % 10) + '0');
+			_putchar(':');
+			_putchar((minutes / 10) + '0');
+			_putchar((minutes % 10) + '0');
+			_putchar(':');
+			_putchar('\n');
+		}
+	}
+}
