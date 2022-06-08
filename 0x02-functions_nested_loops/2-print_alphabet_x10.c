215752aef6bf998c62262ec3fb6b0f5c5352dd97
BOUBEY <hamma.boubey@gmail.com>
2022-06-07 20:12:58 -0700

    0x02 task 2

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * print_alphabet_x10 - Printing the alphabet 10 times, lowercase
+ *
+ */
+void print_alphabet_x10(void)
+{
+	char letter = 'a';
+	int n;
+
+	while (n <= 10)
+	{
+		while (letter <= 'z')
+		{
+			_putchar(letter);
+			if (letter == 'z')
+			{
+				_putchar('\n');
+			}
+			letter++;
+		}
+		n++;
+	}
+}
