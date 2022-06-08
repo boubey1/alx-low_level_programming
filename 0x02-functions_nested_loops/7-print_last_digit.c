00226a5e8cd09e3a1512600c9c88283d875ad278
BOUBEY <hamma.boubey@gmail.com>
2022-06-07 22:07:37 -0700

    0x02 task 7

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,11 @@
+#include "main.h"
+
+/**
+ * print_last_digit - Print the last digit
+ * @n: the value to be returned
+ * Return: value of the int, 
+ */
+int print_last_digit(int n)
+{
+	return (n % 10);
+}
