bceaf7363c2c80a680271f9bb7546896dbdd84ee
BOUBEY <hamma.boubey@gmail.com>
2022-06-07 20:53:17 -0700

    0x02 task 2

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -8,12 +8,11 @@ void print_alphabet_x10(void)
 {
 	char letter = 'a';
 	int n = 0;
-	
 	while (n++ <= 9)
 	{
 		for (letter = 'a'; letter <= 'z'; letter++)
 		{
-		       	_putchar(letter);
+			_putchar(letter);
 		}
 	_putchar('\n');
 	}
