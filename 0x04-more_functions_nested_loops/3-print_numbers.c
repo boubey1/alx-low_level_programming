0e3ff435f91bfef36c6dfce0872281717b35a661
BOUBEY <hamma.boubey@gmail.com>
2022-06-09 17:51:46 -0700

    0x04 task3

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * print_numbers - display the numbers
+ */
+void print_numbers(void)
+{
+	int number;
+
+	for (number = 0; number <= 9; number++)
+	{
+		_putchar((number % 10) + '0');
+	}
+	_putchar('\n');
+}
