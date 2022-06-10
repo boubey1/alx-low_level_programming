6294bb31c55b7f63ed506691d6080f40bfad6c6e
BOUBEY <hamma.boubey@gmail.com>
2022-06-09 18:36:38 -0700

    0x04 task5

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * more_numbers - prints 10 times the numbers 0 to 14
+ */
+void more_numbers(void)
+{
+	int number = 0;
+	int xtime = 0;
+
+	while (xtime <= 10)
+	{
+		for (number = 0; number <= 14; number++)
+		{
+			_putchar((number % 10) + '0');
+		}
+
+		_putchar('\n');
+		xtime++;
+	}
+	_putchar('\n');
+}
+
