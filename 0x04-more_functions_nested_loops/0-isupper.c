ae4adae588f0b58790c050cd65a0ff1e272b7c06
BOUBEY <hamma.boubey@gmail.com>
2022-06-09 17:01:30 -0700

    0x04 task0

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -3,7 +3,7 @@
 /**
  * _isupper - it checks if the letter is uppercase
  * @c: the variable to be checked
- * Return: returns the value of c (0 ou 1)
+ * Return: returns 0 ou 1
  */
 int _isupper(int c)
 {
@@ -11,11 +11,10 @@ int _isupper(int c)
 
 	if (c >= 'A' && c <= 'Z')
 	{
-		c = 1;
+		return (1);
 	}
 	else
 	{
-		c = 0;
+		return (0);
 	}
-	return (c);
 }
