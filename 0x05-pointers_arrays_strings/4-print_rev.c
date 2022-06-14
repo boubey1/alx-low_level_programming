645a08a5d7dd8bd258db4365490073b73985bc06
BOUBEY <hamma.boubey@gmail.com>
2022-06-13 17:59:23 -0700

    0x05 task 4

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * print_rev - prints a string.
+ * @s: the string value to print.
+ */
+void print_rev(char *s)
+{
+	int i;
+
+	while (s[i] != '\0')
+	{
+		i++;
+	}
+	i--;
+	while (s[i] != '\0')
+	{
+		_putchar(s[i--]);
+	}
+	_putchar('\n');
+}
