0f1eea70de794495069f07c97f511f8a0fd7f72a
BOUBEY <hamma.boubey@gmail.com>
2022-06-13 17:03:26 -0700

    0x05 task 0

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,11 @@
+#include "main.h"
+
+/**
+ * reset_to_98 - manage to give a pointer to int 98
+ * @n: the pointer.
+ * Return: void
+ */
+void *reset_to_98(int *n)
+{
+	*n = 98;
+}
---
+++ 0x05-pointers_arrays_strings/main.h
@@ -0,0 +1,12 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+#include <stdio.h>
+
+/*
+ * Define the main.h as header
+ */
+
+void *reset_to_98(int *n);
+
+#endif
