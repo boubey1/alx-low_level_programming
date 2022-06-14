04331255e94fb02dfe50755769b1e1ebf6caa024
BOUBEY <hamma.boubey@gmail.com>
2022-06-13 19:08:49 -0700

    0x05 task 9

---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * _strcpy - cpy strings 
+ * @dest: value to copy the string to.
+ * @src: The source string to copy.
+ *
+ * Return: dest
+ */
+char *_strcpy(char *dest, const char *src)
+{
+	int index = 0;
+
+	while (src[index])
+	{
+		dest[index] = src[index];
+		index++;
+	}
+
+	return (dest);
+}
