08a37f7b6ee8e2e0cb7f3621d75284ff63dc1839
boubey1 <hamma.boubey@gmail.com>
2023-07-01 16:19:45 -0700

    first commit task 1

---
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -0,0 +1,22 @@
+#include "lists.h"
+
+/**
+ * list_len - a function to search the number of 
+ *            elements in a given linked list_t list.
+ * @h: argument of the list_t list.
+ *
+ * Return: retrun the number  nb of elements found in h.
+ */
+size_t list_len(const list_t *h)
+{
+	size_t nb = 0;
+
+	while (h)
+	{
+		nb++;
+		h = h->next;
+	}
+
+	return (nb);
+}
+
