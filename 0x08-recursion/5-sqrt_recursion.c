77aa92ba904636c3a86d0a6897143204b3fe43ca
boubey1 <hamma.boubey@gmail.com>
2023-07-05 16:30:36 -0700

    my checked from files for 0423 cohort

---
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -0,0 +1,40 @@
+#include "main.h"
+
+
+/**
+ * square_of_nb - a function to determine the square root
+ * @nb: The initial number to look for the square root
+ * @init_root: The root
+ *
+ * Return: the value of the root, if not -1
+ */
+int square_of_nb(int nb, int init_root)
+{
+	if ((init_root * init_root) == nb)
+		return (init_root);
+
+	if (init_root == nb / 2)
+		return (-1);
+
+	return (square_of_nb(nb, init_root + 1));
+}
+
+/**
+ * _sqrt_recursion - gives the value of square root of a given number
+ * @n: The initial number to look for the square root
+ *
+ * Return: the value of the root, if not -1
+ */
+int _sqrt_recursion(int n)
+{
+	int init_root = 0;
+
+	if (n < 0)
+		return (-1);
+
+	if (n == 1)
+		return (1);
+
+	return (square_of_nb(n, init_root));
+}
+
--- 0x08-recursion/main.h
+++ 0x08-recursion/main.h
@@ -11,7 +11,20 @@
 int _putchar(char c);
 void _puts_recursion(char *s);
 void _print_rev_recursion(char *s);
-
+int _strlen_recursion(char *s);
+int factorial(int n);
+int _pow_recursion(int x, int y);
+int _sqrt_recursion(int n);
+int square_of_nb(int nb, int init_root);
+int prime_div_check(int nb, int prime_divisor);
+int is_prime_number(int n);
+int is_palindrome(char *s);
+int val_strlen(char *s);
+int test_palindrome(char *s, int length, int addr);
+int wildcmp(char *s1, char *s2);
+int strlen_esc_wild_c(char *str);
+void find_no_wild_c(char **wild_str);
+char *check_for_wild(char *str, char *prev);
 
 #endif
 
