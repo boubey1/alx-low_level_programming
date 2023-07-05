f37285f7d6fe15b77e858f111c4803ee5900f8d2
boubey1 <hamma.boubey@gmail.com>
2023-07-05 16:12:41 -0700

    task 0 and readme file

---
+++ 0x08-recursion/main.h
@@ -0,0 +1,16 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+#include <unistd.h>
+
+/**
+ * HEADER: main.h 
+ * Description: Holds all the prototypes used
+ */
+
+int _putchar(char c);
+void _puts_recursion(char *s);
+
+
+#endif
+
