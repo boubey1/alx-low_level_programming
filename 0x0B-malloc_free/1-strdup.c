3ddad451314d6d2276664f54b48299b4cb35491c
boubey1 <hamma.boubey@gmail.com>
2023-11-22 05:46:19 +0000

    changes betty and memory

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -5,46 +5,42 @@
  * _strdup : a function to returns a pointer for an allocated
  * memory
  * @str: the string to whom a memory mush be allocated
- * 
+ *
  * Return: the address of the memory allocated
  */
 
 char *_strdup(char *str)
 {
-    
-    int i = 0;
-    int len = i + 1;
-    char *str_returned;
 
-    if(str == NULL)
-    {
+   int i = 0;
+   char *str_returned;
+
+   if(str == NULL)
+   {
+       return(NULL);
+   }
+
+   while(str[i] != '\0')
+   {
+       i++;
+   }
+
+   str_returned = malloc(sizeof(char) * (i + 1));
+
+   if(str_returned == NULL)
+   {
         return(NULL);
-    }
+   }
 
-    while(str[i] != '\0')
-    {
-        i++;
-    }
+   i = 0;
 
-    str_returned = malloc(sizeof(char) * len);
+   while(str[i] != '\0')
+   {
+       str_returned[i] = str[i];
+       i++;
+   }
+       str_returned[i] = '\0';
 
-    if(str_returned == NULL)
-    {
-        return(NULL);
-    }
-
-    i = 0;
-
-    while(str[i] != '\0')
-    {
-        str_returned[i] = str[i];
-        i++;
-    }
-/*
-    for (i = 0; str[i]; i++)
-		str_returned[i] = str[i];
-
-*/  
-    return(str_returned);
+   return(str_returned);
 
 }
