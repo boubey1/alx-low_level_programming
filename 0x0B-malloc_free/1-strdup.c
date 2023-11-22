bf9e1466a5a9470b37062ab528067d44995f5e25
boubey1 <hamma.boubey@gmail.com>
2023-11-22 23:45:28 +0000

    Betty task 1

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -2,15 +2,17 @@
 #include <stdlib.h>
 
 /**
- * _strdup : a function to returns a pointer for an allocated
+ * _strdup: a function to returns a pointer for an allocated
  * memory
  * @str: the string to whom a memory mush be allocated
  *
+ * Desc: RAS
  * Return: the address of the memory allocated
  */
 
 char *_strdup(char *str)
 {
+
 int i = 0;
 char *str_returned;
 
@@ -19,22 +21,22 @@ return (NULL);
 
 while (str[i] != '\0')
 {
-    i++;
+i++;
 }
 
 str_returned = malloc(sizeof(char) * (i + 1));
 
 if (str_returned == NULL)
 {
-    return (NULL);
+return (NULL);
 }
 
 i = 0;
 
 while (str[i] != '\0')
 {
-    str_returned[i] = str[i];
-    i++;
+str_returned[i] = str[i];
+i++;
 }
 str_returned[i] = '\0';
 
