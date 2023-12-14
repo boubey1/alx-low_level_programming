570cde181dd21e75e2d9a585efb3c6abb7d59de4
boubey1 <hamma.boubey@gmail.com>
2023-12-14 22:43:59 +0000

    task 3, -1 output return fixing

--- 0x14-bit_manipulation/3-set_bit.c
+++ 0x14-bit_manipulation/3-set_bit.c
@@ -13,11 +13,11 @@ int set_bit(unsigned long int *n, unsigned int index)
 	unsigned int size;
 
 	size = sizeof(unsigned long int) * 8;
-	
-	*n ^= (1 << index);
 
 	if (index >= size)
 		return (-1);
+
+	*n ^= (1 << index);
 
 	return (1);
 }
