5a8d0272305d84a095e07ead188bfe1ea60e9200
boubey1 <hamma.boubey@gmail.com>
2023-12-14 21:52:44 +0000

    commit task 2, all checks done

---
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * get_bit -  a function that returns the value of a bit at a given index
+ * @n: bit given as parameter
+ * @index: index of the value
+ *
+ * Return: If error returns -1, if success the value of the bit
+ */
+int get_bit(unsigned long int n, unsigned int index)
+{
+
+	if ((n & (1 << index)) == 0)
+		return (0);
+
+	else
+		return (1);
+
+}
